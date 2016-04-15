bb6ad5e26222e40d751b01d49a12d87472dc5308
Buntworthy <justinpinkney@gmail.com>
2016-04-15 12:41:23 +0100

    Python running script

---
+++ runner.py
@@ -0,0 +1,11 @@
+options = {
+	'sort': ('R', 'G', 'B', 'Hue', 'Saturation', 'Brightness', 'Shuffle')
+	'random': range(10)/10.0
+	'distance': ('RGB', 'HSB')
+	'preset': ('Centre', 'Corner', 'Border', 'Diagonal', 'Random')
+}
+
+resolution = (1920, 1080)
+
+file_directory = 'root_directory'
+files = ('im1.jpg', 'etc')
