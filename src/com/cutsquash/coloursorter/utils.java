3ed54eefd436e77953febbabac605209a50128ad
Buntworthy <justinpinkney@gmail.com>
2016-04-15 19:41:45 +0100

    utils

--- src/com/cutsquash/coloursorter/utils.java
+++ src/com/cutsquash/coloursorter/utils.java
@@ -14,4 +14,13 @@ public class Utils {
 
         return Color.RGBtoHSB(r1, g1, b1, null);
     }
+
+    public static int[] getRgb(int c) {
+        int r1 = (c>>16 & 0xFF);
+        int g1 = (c>>8 & 0xFF);
+        int b1 = (c & 0xFF);
+
+        return new int[]{r1, g1, b1};
+    }
+
 }
